571ab33dff995451bfb8d9d8eb9025dec570d4c7
Roeyyoo <royazlif@gmail.com>
2024-12-27 07:35:32 +0700

    Update user_repo.dart

--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -11,9 +11,3 @@ abstract class UserRepository {
 
   Future<void> logOut();
 }
-// ini udah kode bawaan paste aja, <MyUser> itu class, jadi nanti harus bikin classnya
-// we will use it as a method
-//we implements those method in firebase user repo
-
-//(2)
-// di sini ada eroro karena belum bikin MyUser class-nya
